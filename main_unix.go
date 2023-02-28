c1a6a1f5e5115086c0a184cd59abce677a34694a
Matt LaPlante <mattl@google.com>
2023-02-28 09:53:29 -0800

    Add multi-platform tests.
    PiperOrigin-RevId: 512963026

--- main_unix.go
+++ main_unix.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build linux
+//go:build linux || darwin
 
 package main
 
@@ -24,4 +24,3 @@ func setup() error {
 func run() error {
 	return nil
 }
-
